2296d9bf1f43ccfca1a01b659e5f4424ac914f69
= <dasgoodase@gmail.com>
2021-04-16 12:21:05 +0400

    add jshintrc and add object in sripts

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?');
+
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+};
