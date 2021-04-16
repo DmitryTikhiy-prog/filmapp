6c245121225e3303e6742e3cc04730d5b5e157b5
= <dasgoodase@gmail.com>
2021-04-16 15:35:36 +0400

    second task in scripts

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?');
 
 const personalMovieDB = {
@@ -8,7 +10,26 @@ const personalMovieDB = {
     privat:false
 };
 
-const lastSeeFilm = prompt('Один из последних просмотренных фильмов?'),
-      rateFilm = prompt('На сколько оцените его?');
-personalMovieDB.movies[lastSeeFilm] = rateFilm;
-alert(personalMovieDB.movies[lastSeeFilm]);
+for (let i= 0; i < 2; i++) {
+    let lastSeeFilm = prompt('Один из последних просмотренных фильмов?'),
+        rateFilm = prompt('На сколько оцените его?');
+    if (lastSeeFilm !='' && rateFilm !='' && lastSeeFilm !=null && rateFilm != null && lastSeeFilm.length<50) {
+        personalMovieDB.movies[lastSeeFilm] = rateFilm;
+        alert(personalMovieDB.movies[lastSeeFilm]);
+    } else {
+        alert('Неправильный ввод');
+        i--;
+    }
+    
+}
+
+
+if (personalMovieDB.count<10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB>=10 && personalMovieDB<30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB>=30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
